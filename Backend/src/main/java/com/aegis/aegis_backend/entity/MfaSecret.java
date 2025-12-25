eee917b4fda2ba083da2c5c902ea0fbab8a28d54
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-12-25 21:17:29 +0530

    feat: Add MfaSecret entity and repository for multi-factor authentication

--- Backend/src/main/java/com/aegis/aegis_backend/entity/MfaSecret.java
+++ Backend/src/main/java/com/aegis/aegis_backend/entity/MfaSecret.java
@@ -1,5 +1,34 @@
 package com.aegis.aegis_backend.entity;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "mfa_secrets")
 public class MfaSecret {
-    
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String userId;
+
+    @Column(nullable = false)
+    private String encryptedString;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public enum Status {
+        ACTIVE,
+        PENDING,
+        REVOKED,
+    }
+
 }
